f439a83b9098e1cd0c40633dead277a780c81a49
Petter Karlsrud <ptrkrlsrd@users.noreply.github.com>
2025-01-14 21:44:31 +0100

    test

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/vendor/keymap.c
@@ -74,7 +74,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case L_BRACER:
         if (record->event.pressed) {
-            SEND_STRING("{");
+            SEND_STRING("{ ");
         } else {
             // when keycode L_BRACER is released
         }
