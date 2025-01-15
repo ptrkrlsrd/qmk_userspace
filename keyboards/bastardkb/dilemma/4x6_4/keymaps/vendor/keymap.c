cfb68cb6c6888c88e32d5755261e041f5388062f
ptrkrlsrd <ptrkrlsrd@users.noreply.github.com>
2025-01-15 23:49:09 +0100

    Fix comma

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/vendor/keymap.c
@@ -105,7 +105,7 @@ enum {
 
 tap_dance_action_t tap_dance_actions[] = {
     [TD_GRV_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC),
-    [CT_CLN] = ACTION_TAP_DANCE_TAP_HOLD(KC_COMM, KC_SCLN),
+    [CT_CLN] = ACTION_TAP_DANCE_TAP_HOLD(KC_COMM, S(KC_COMM)),
 };
 
 enum custom_keycodes {
