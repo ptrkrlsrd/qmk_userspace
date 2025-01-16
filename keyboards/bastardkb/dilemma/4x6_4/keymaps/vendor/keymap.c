ba225ed74a004e3f88b95a570fd106257066d61b
ptrkrlsrd <ptrkrlsrd@users.noreply.github.com>
2025-01-16 08:51:37 +0100

    Add break

--- keyboards/bastardkb/dilemma/4x6_4/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4/keymaps/vendor/keymap.c
@@ -143,6 +143,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
             tap_code16(tap_hold->tap);
         }
+        break;
     }
     return true;
 };
